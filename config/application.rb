93134cacfc8d91e1943177629afa5d43d9507484
thooba siddiqa thooba.siddiqa@gmail.com <thooba.siddiqa@gmail.com>
2023-03-01 16:19:11 +0100

    asset precompilation

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module BookSharing
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Configuration for the application, engines, and railties goes here.
     #
