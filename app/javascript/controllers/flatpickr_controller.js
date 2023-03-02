1eac394d993f564e7f6535ca9ec58969bd25603d
thooba siddiqa thooba.siddiqa@gmail.com <thooba.siddiqa@gmail.com>
2023-03-02 22:44:35 +0100

    setup date picker

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,7 +4,7 @@ import flatpickr from "flatpickr";
 export default class extends Controller {
   static targets = [ "reservation_start", "reservation_end" ]
   connect() {
-    flatpickr(this.reservation_startTarget, {})
-    flatpickr(this.reservation_endTarget, {})
+    flatpickr(this.reservation_startTarget, {defaultDate: "today"})
+    flatpickr(this.reservation_endTarget, {defaultDate: "today"})
   }
 }
